c21957304c313da568c0d1ce5d0dacb32f0b6bfa
BenAvrahamos <benavraham1998@gmail.com>
2024-03-16 19:59:22 +0200

    Attempted to resolve issue with photos in about

--- views/About.jsx
+++ views/About.jsx
@@ -8,12 +8,12 @@ export function About() {
 In Techlandia, Yossef and Ben tackled Sprint 3, battling bugs and code chaos. With determination and teamwork, they triumphed, proving that together, anything is possible in software development.</p>
         <section>
             <div>
-                <img className="profile" src="assets\img\Ben.PNG" alt="" />
+                <img className="profile" src="assets\img\Ben.png" alt="" />
                 <p>
 Ben, the visionary virtuoso of innovation, dances through the digital landscape with boundless creativity and flair. With a mind as agile as his fingers on the keyboard, he conjures ingenious solutions to perplexing problems, infusing every line of code with a spark of brilliance. His passion for pushing boundaries and crafting groundbreaking software makes him a luminary in the world of technology.</p>
             </div>
             <div>
-                <img className="profile" src="assets\img\Yossef.PNG" alt="" />
+                <img className="profile" src="assets\img\Yossef.png" alt="" />
                 <p>
 Yossef, the stalwart software savant, navigates the digital realm with precision and finesse. Armed with an insatiable curiosity and a keen intellect, he fearlessly confronts complex coding challenges, unraveling them with ease. His unwavering determination and meticulous attention to detail make him a true hero of the digital age, inspiring awe and admiration in all who witness his exploits.</p>
             </div>
